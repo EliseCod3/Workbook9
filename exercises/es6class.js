8449347db940e6fdc44765968eedec63ff294c67
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-21 14:00:36 -0500

    finished exercise 2 and 3

--- exercises/es6class.js
+++ exercises/es6class.js
@@ -10,6 +10,15 @@ class Employee {
     fullName() {
         return `${this.firstName} ${this.lastName}`
     }
+
+    promote(newJobTitle, newPayRate) {
+        this.jobTitle = newJobTitle;
+        this.payRate = newPayRate;
+    }
+
+    getIntro(){
+        return `Hi my name is ${this.firstName} ${this.lastName}, I'm a ${this.jobTitle}.`
+    }
 }
 
 // let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.50);
@@ -20,7 +29,23 @@ let employee1 = new Employee (1, "Ian", "Auston", "Graphic Artist", 42.50);
     console.log(`Job title is ${employee1.jobTitle}`);
     console.log(`Pay rate is $${employee1.payRate}`);
 
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 let employee2 = new Employee (2, "Ray", "Bell", "Software Developer", 63.93);
     console.log(`Employee ${employee2.fullName()} created`);
     console.log(`Job title is ${employee2.jobTitle}`);
     console.log(`Pay rate is $${employee2.payRate}`);
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+// Exercise 2
+employee1.promote("Sr. Graphic Artist", 46.75);
+
+    console.log(`Job title is ${employee1.jobTitle}`); 
+    console.log(`Pay rate is $${employee1.payRate}`);
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+// Exercise 3
+let intro = employee1.getIntro();
+console.log(intro);
