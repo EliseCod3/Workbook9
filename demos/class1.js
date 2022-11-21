0ba13f1d54b4b8051f9475319460b9ba0ca91842
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-21 11:35:37 -0500

    couple class examples

---
+++ demos/class1.js
@@ -0,0 +1,37 @@
+let person1 = {
+    first: 'Ray',
+    last: 'Bell',
+    email: 'nottoday@gmail.com'
+
+}
+console.log(person1);
+
+let person2 = {
+    first: 'Ari',
+    last: 'Black',
+    email: 'blackisback@gmail.com'
+
+}
+
+console.log(person2);
+
+class Person {
+    // first;
+    // last;
+    // email;
+
+    constructor(first, last, email) {
+        this.first = first;
+        this.last = last;
+        this.email = email;
+    }
+}
+
+// let person3 = new Person();
+// person3.first = "Rayna";
+// person3.last = "Bell";
+// person3.email = "eliseraynehasawsomesauce@gmail.com"
+let person3 = new Person('Rayna', 'Bell', 'ringmybell@gmail.com')
+
+console.log(person3);
+
